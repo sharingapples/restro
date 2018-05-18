04c0b14892484fd831aea47cc7440d9000210126
Bhagya Sah <bhagyasah4u@gmail.com>
2018-05-18 16:42:08 +0545

    Fix escape character

--- packages/server/src/db/findOne.js
+++ packages/server/src/db/findOne.js
@@ -1,6 +1,6 @@
 export default async function find(db, table, condition) {
   if (typeof condition === 'number' || typeof condition === 'string') {
-    return db.get(`SELECT * FROM '${table}' WHERE id=?`, condition);
+    return db.get(`SELECT * FROM [${table}] WHERE [id]=?`, condition);
   }
 
   const fields = Object.keys(condition);
@@ -8,6 +8,5 @@ export default async function find(db, table, condition) {
   const params = fields.map(f => condition[f]);
 
   const sql = `SELECT * FROM [${table}] WHERE ${conditionStr}`;
-  console.log(sql, params);
   return db.get(sql, ...params);
 }
--- packages/server/src/db/update.js
+++ packages/server/src/db/update.js
@@ -8,7 +8,6 @@ export default async function update(db, table, values, condition) {
   const conditionParams = conditionFields.map(c => condition[c]);
 
   const sql = `UPDATE [${table}] SET ${set} WHERE ${conditionStr}`;
-  console.log(sql, fieldParams, conditionParams);
   const res = await db.run(sql, ...fieldParams, ...conditionParams);
   return res.changes;
 }
