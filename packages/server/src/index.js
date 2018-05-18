995466b0d59b5b42fcafe7a6a72ff171ab506ac0
Ranjan Shrestha <ranjan@sharingapples.com>
2018-05-18 13:16:49 +0545

    Add express

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -1,8 +1,14 @@
+import express from 'express';
+import http from 'http';
 import start from 'redsock';
 
 import { validateToken } from './auth';
 
-start({ port: 8080, url: '/:origin/:token' }, (session) => {
+const app = express();
+const server = http.createServer(app);
+
+// Start the websocket server
+start({ server, url: '/redsock/:origin/:token' }, (session) => {
   // Check if its a valid user
   const user = validateToken(session.params.token);
   session.set('user', user);
@@ -10,3 +16,6 @@ start({ port: 8080, url: '/:origin/:token' }, (session) => {
   // Based on the type of the user, join specific channels
 
 });
+
+// Start the http server
+server.listen(8080);
