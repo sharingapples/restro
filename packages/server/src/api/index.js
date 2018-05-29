892c0ec654851427ddeb4c5604de552c123f355b
Ranjan Shrestha <ranjan@sharingapples.com>
2018-05-29 12:34:21 +0545

    Use general scope for generic actions

--- packages/server/src/api/index.js
+++ packages/server/src/api/index.js
@@ -1,22 +1,30 @@
 import { createScope } from 'shocked';
 import switchRestro from './switchRestro';
 
+import changePassword from './changePassword';
 import logout from './logout';
+
 import placeOrder from './placeOrder';
 import cancelOrder from './cancelOrder';
 import printOrder from './printOrder';
 
-const admin = createScope('Admin', () => {
+const general = createScope('General', () => {
 
 });
-admin(logout);
+general(logout);
+general(changePassword);
+
+// const admin = createScope('Admin', () => {
+
+// });
+// admin(logout);
+// admin(changePassword);
 
 const waiter = createScope('Waiter', () => {
   // TODO: Make sure the session belongs to a waiter
 });
 
 waiter(placeOrder);
-waiter(logout);
 
 const cashier = createScope('Cashier', () => {
 
@@ -24,12 +32,6 @@ const cashier = createScope('Cashier', () => {
 
 cashier(cancelOrder);
 cashier(printOrder);
-cashier(logout);
 
-export {
-  admin,
-  waiter,
-  cashier,
-
-  switchRestro,
-};
+// eslint-disable-next-line import/prefer-default-export
+export { switchRestro };
