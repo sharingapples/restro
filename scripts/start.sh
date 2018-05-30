11dcf7a6de4389f52302676999ceda1fc37d1a13
Ranjan Shrestha <ranjan@sharingapples.com>
2018-05-30 14:01:27 +0545

    Handle app start error in case the app is already running

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 source /home/ec2-user/.bash_profile
 
+# Stop the app (Ignore error if the app is not registered)
+pm2 stop app || true
+
+# Start the app
 cd /home/ec2-user/restro/packages/server
 pm2 start build/index.js -n app
 
