87ed6ba135aa9a77fd6a024a0840b29a49820f4d
Ranjan Shrestha <ranjan@sharingapples.com>
2018-05-30 13:34:57 +0545

    Fix pm2 global installation (Use npm instead of yarn)

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,12 +3,12 @@ source /home/ec2-user/.bash_profile
 
 # First install node
 curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
-sudo yum -y install nodejs
+yum -y install nodejs
 
 # Install Yarn
 curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
-sudo yum -y install yarn
+yum -y install yarn
 
 # Install pm2
-sudo yarn global add pm2
+npm install -g pm2
 pm2 update
