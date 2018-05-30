8a2a8af801835c6406bafee249bfc894d2e56861
Ranjan Shrestha <ranjan@sharingapples.com>
2018-05-30 15:02:12 +0545

    Forward all http request to https forwarded via AWS ELB

--- packages/server/src/express/index.js
+++ packages/server/src/express/index.js
@@ -6,6 +6,16 @@ import resetPassword from './resetPassword';
 import forgotPassword from './forgotPassword';
 
 export default function (app) {
+  // Force to https
+  app.use((req, res, next) => {
+    const proto = req.get('X-Forwarded-Proto');
+    if (proto && proto !== 'https') {
+      res.redirect(301, `https://${req.get('Host')}${req.url}`);
+    } else {
+      next();
+    }
+  });
+
   app.get('/auth/login', login);
   app.get('/auth/reset', resetPassword);
   app.get('/auth/forgot', forgotPassword);
