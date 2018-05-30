d70edb5e89787040c64e465a59e282b49d291749
Ranjan Shrestha <ranjan@sharingapples.com>
2018-05-30 11:37:29 +0545

    Trim cookie names

--- packages/web/src/redux/account/cookies.js
+++ packages/web/src/redux/account/cookies.js
@@ -2,24 +2,22 @@
 
 export default {
   get: (cookie) => {
-    const chk = `${cookie}=`;
+    const chk = `${cookie.trim()}=`;
     const cookies = document.cookie.split(';');
-    console.log(cookies);
     const r = cookies.find(c => c.trim().indexOf(chk) === 0);
     if (!r) {
       return null;
     }
 
     const res = r.trim().substr(chk.length);
-    console.log(res);
     return res;
   },
 
   set: (cookie, value) => {
-    document.cookie = `${cookie}=${value}`;
+    document.cookie = `${cookie.trim()}=${value}`;
   },
 
   clear: (cookie) => {
-    document.cookie = `${cookie}= ;expires=${(new Date(Date.now() - 1)).toUTCString()}`;
+    document.cookie = `${cookie.trim()}= ;expires=${(new Date(Date.now() - 1)).toUTCString()}`;
   },
 };
