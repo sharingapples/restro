7f3d07ea85895faae92805e47c961c0ae017d3aa
Ranjan Shrestha <ranjan@sharingapples.com>
2018-05-30 13:44:10 +0545

    Fix protocol conversion from https -> wss

--- packages/web/src/socket/getRemoteUrl.js
+++ packages/web/src/socket/getRemoteUrl.js
@@ -1,7 +1,7 @@
 /* global window */
 import store from '../redux';
 
-const protocol = process.env.REACT_APP_SOCKET_PROTOCOL || window.location.protocol === 'https' ? 'wss' : 'ws';
+const protocol = process.env.REACT_APP_SOCKET_PROTOCOL || (window.location.protocol === 'https:' ? 'wss' : 'ws');
 const host = process.env.REACT_APP_SOCKET_HOST || window.location.hostname;
 const port = process.env.REACT_APP_SOCKET_PORT || window.location.port;
 
