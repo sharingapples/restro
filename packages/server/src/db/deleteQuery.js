b16b17753e2ee04bda0f129f2c08b30fa758ce4e
Ranjan Shrestha <ranjan@sharingapples.com>
2018-06-03 07:44:44 +0545

    Fix delete query

--- packages/server/src/db/deleteQuery.js
+++ packages/server/src/db/deleteQuery.js
@@ -1,9 +1,10 @@
 
 export default async function deleteQuery(db, table, condition) {
   const fields = Object.keys(condition);
-  const conditionStr = fields.map(f => `'${f}'=?`).join(' AND ');
+  const conditionStr = fields.map(f => `[${f}]=?`).join(' AND ');
   const params = fields.map(f => condition[f]);
 
-  const res = await db.run(`DELETE FROM '${table}' WHERE ${conditionStr}`, ...params);
-  return res.changes;
+  const sql = `DELETE FROM [${table}] WHERE ${conditionStr}`;
+  const res = await db.run(sql, ...params);
+  return res.stmt.changes;
 }
