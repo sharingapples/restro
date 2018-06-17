7a8fdd47eb7079200d162da58fe66af0bcd2ab51
Ranjan Shrestha <ranjan@sharingapples.com>
2018-06-17 17:16:11 +0545

    Use heartbeat to detect connection

--- packages/printer/src/index.js
+++ packages/printer/src/index.js
@@ -4,43 +4,55 @@ const config = require('../config.json');
 const formatBill = require('./formatBill');
 const formatOrder = require('./formatOrder');
 const connect = require('shocked-client').default;
-const createNetwork = require('shocked-network-node').default;
+// const createNetwork = require('shocked-network-node').default;
 const WebSocket = require('ws');
 
-const network = createNetwork({ domain: 'restro.net' });
+// const network = createNetwork({ domain: 'restro.net' });
 
 const client = connect(
   `${config.server}/shocked/printer/${config.modes}`,
   null,
-  WebSocket,
-  network
+  WebSocket
 );
 
-client.on('error', () => {
-  // Reconnect in a second, if an error occurs
-  setTimeout(() => {
-    client.reconnect();
-  }, 1000);
+let isConnected = false;
+client.on('connect', () => {
+  isConnected = true;
 });
 
-client.on('disconnect', () => {
-  // Retry connecting in a second, if disconnected
-  setTimeout(() => {
+// Add a heart beat to run every 3 seconds to make sure we are connected
+setInterval(async () => {
+  if (!isConnected) {
+    // Close the client connection and try to reconnect
+    console.log(moment().format('YYYY-MM-DD HH:mm:ss'), 'Client is not connected. Retry.');
     client.reconnect();
-  }, 1000);
-});
+    return;
+  }
+
+  // Reset the flag, before pinging
+  isConnected = false;
+  if (client.isConnected()) {
+    try {
+      const p = await client.scope('Printer');
+      await p.ping();
+      isConnected = true;
+    } catch (err) {
+      // Ignore error
+    }
+  }
+}, 3000);
 
 printer.init(config);
 
-network.on('online', () => {
-  console.log('Connecting at', moment().format('HH:mm:ss'));
-  // Perform a reconnect as soon as we get a online signal
-  client.reconnect();
-});
+// network.on('online', () => {
+//   console.log('Connecting at', moment().format('HH:mm:ss'));
+//   // Perform a reconnect as soon as we get a online signal
+//   client.reconnect();
+// });
 
-network.on('offline', () => {
-  console.log('Got offline at', moment().format('HH:mm:ss'));
-});
+// network.on('offline', () => {
+//   console.log('Got offline at', moment().format('HH:mm:ss'));
+// });
 
 async function print() {
   return new Promise((resolve, reject) => {
